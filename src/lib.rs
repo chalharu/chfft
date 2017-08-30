0a12b9813e746011350d8baf3501cbe8cdefe2ce
Mitsuharu Seki <mitsu1986@gmail.com>
2017-08-31 08:01:55 +0900

    modify Cargo.toml & License Text

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,9 @@
 //! complex-to-complex transforms.
 //!
 //! # Licensing
-//!　This Source Code is subject to the terms of the Mozilla Public License
-//!　version 2.0 (the "License"). You can obtain a copy of the License at
-//!　http://mozilla.org/MPL/2.0/.
+//! This Source Code is subject to the terms of the Mozilla Public License
+//! version 2.0 (the "License"). You can obtain a copy of the License at
+//! http://mozilla.org/MPL/2.0/.
 
 extern crate num_traits;
 extern crate num;
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,7 +1,7 @@
 //! # Licensing
-//!　This Source Code is subject to the terms of the Mozilla Public License
-//!　version 2.0 (the "License"). You can obtain a copy of the License at
-//!　http://mozilla.org/MPL/2.0/.
+//! This Source Code is subject to the terms of the Mozilla Public License
+//! version 2.0 (the "License"). You can obtain a copy of the License at
+//! http://mozilla.org/MPL/2.0/.
 
 extern crate chfft;
 extern crate rand;
