41af9c5d4026384b8e24296f54080f9e3dba868d
Mitsuharu Seki <mitsu1986@gmail.com>
2017-10-26 03:41:17 +0900

    modify Cft1d Test

--- src/cfft1d.rs
+++ src/cfft1d.rs
@@ -618,8 +618,9 @@ mod tests {
 
     #[test]
     fn f64_primes() {
+        let mut dft = CFft1D::<f64>::new();
         for &i in &[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97] {
-            test_with_len(&mut CFft1D::<f64>::new(), i);
+            test_with_len(&mut dft, i);
         }
     }
 }
